591c203ee2a36fca33f1be443c18814a8de5292a
Vinicius Galhardo Machado <vinicius.galhardo.ext@onlinepan.com.br>
2024-02-29 08:43:25 -0400

    add baseUlr in tsconfig, to make absolute imports

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,5 +1,6 @@
+import database from 'infra/database'
 import { NextApiRequest, NextApiResponse } from 'next'
-import database from '../../../../infra/database'
+
 export default async function status(req: NextApiRequest, res: NextApiResponse) {
   const result = await database.query('SELECT 1 + 1 as sum;')
   console.log(result)
